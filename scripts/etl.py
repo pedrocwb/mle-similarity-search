84b688061bd55ef2d7e9e483f16766c1ca56bed1
pedro <pedro@datarevenue.com>
2022-08-06 18:44:06 -0300

    create ETL script to download data and save to CSV and rename quickwit config directory
    Took 53 seconds

--- scripts/etl.py
+++ scripts/etl.py
@@ -1,65 +1,108 @@
-import sys
+import os
 import csv
 import json
-import numpy as np
-from datazeit.config import ROOT_DIR
+import logging
+import sys
 
-#
+import requests
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger("DatazeitETL")
 
 
-def convert_reviews_data_to_json(
-    csv_path: str = f"{ROOT_DIR}/data/cp_ml_eng_reviews.csv",
-):
-    save_path = f"{csv_path.split('.csv')[0]}.json"
+REVIEWS_CSV_URL = (
+    "https://s3.eu-central-1.wasabisys.com/dz-shared/wilhelm/cp_ml_eng_reviews.csv"
+)
+PRODUCTS_CSV_URL = (
+    "https://s3.eu-central-1.wasabisys.com/dz-shared/wilhelm/cp_ml_eng_products.csv"
+)
+INGREDIENTS_CSV_URL = (
+    "https://s3.eu-central-1.wasabisys.com/dz-shared/wilhelm/cp_ml_eng_ingredients.csv"
+)
 
-    with open(save_path, "w+") as json_fp:
-        with open(csv_path) as csv_fp:
-            for entry in csv.DictReader(csv_fp.readlines()):
-                json_entry = json.dumps(
-                    {"p_e_id": entry["p_e_id"], "text": entry["text"]}
-                )
+
+class DatazeitETL:
+    def __init__(self):
+        self._session = requests.session()
+
+    def run(self):
+        logger.info("Running Datazeit ETL.")
+
+        os.makedirs("../data", exist_ok=True)
+
+        self.download_reviews_data()
+        self.download_products_data()
+        self.download_ingredients_data()
+
+        self._session.close()
+        logger.info("Finished running Datazeit ETL.")
+
+    def _download_file(self, url: str):
+        download = self._session.get(url)
+        decoded_content = download.content.decode("utf-8")
+        cr = csv.reader(decoded_content.splitlines(), delimiter=",")
+        return cr
+
+    def download_reviews_data(self):
+        logger.info("Downloading and converting Reviews data")
+        save_path = f"../data/{REVIEWS_CSV_URL.split('/')[-1].replace('.csv', '.json')}"
+        cr = self._download_file(REVIEWS_CSV_URL)
+
+        with open(save_path, "w+") as json_fp:
+            for p_e_id, text in list(cr)[1:]:
+                json_entry = json.dumps({"p_e_id": p_e_id, "text": text})
                 json_fp.write(json_entry)
                 json_fp.write("\n")
 
+        logger.info(f"Finished downloading reviews data. Saved to {save_path}")
 
-def convert_products_to_json(csv_path: str = f"{ROOT_DIR}/data/cp_ml_eng_products.csv"):
-    save_path = f"{csv_path.split('.csv')[0]}.json"
+    def download_products_data(self):
+        logger.info("Downloading and converting Products data")
+        save_path = (
+            f"../data/{PRODUCTS_CSV_URL.split('/')[-1].replace('.csv', '.json')}"
+        )
+        cr = self._download_file(PRODUCTS_CSV_URL)
 
-    with open(save_path, "w+") as json_fp:
-        with open(csv_path) as csv_fp:
-            for entry in csv.DictReader(csv_fp.readlines()):
+        with open(save_path, "w+") as json_fp:
+            for p_c_id, brand, title, product_type, p_e_ids in list(cr)[1:]:
                 json_entry = json.dumps(
                     {
-                        "p_c_id": int(entry["p_c_id"]),
-                        "brand": entry["brand"],
-                        "title": entry["title"],
-                        "product_type": entry["product_type"],
+                        "p_c_id": int(p_c_id),
+                        "brand": brand,
+                        "title": title,
+                        "product_type": product_type,
                         "p_e_ids": [
-                            int(i) for i in eval(entry["p_e_ids"])
+                            int(i) for i in eval(p_e_ids)
                         ],  # transform to list of p_e_ids
                     }
                 )
                 json_fp.write(json_entry)
                 json_fp.write("\n")
 
+        logger.info(f"Finished downloading products data. Saved to {save_path}")
 
-def convert_ingredients_to_json(
-    csv_path: str = f"{ROOT_DIR}/data/cp_ml_eng_ingredients.csv",
-):
-    save_path = f"{csv_path.split('.csv')[0]}.json"
+    def download_ingredients_data(self):
+        logger.info("Downloading and converting Ingredients data")
+        save_path = (
+            f"../data/{INGREDIENTS_CSV_URL.split('/')[-1].replace('.csv', '.json')}"
+        )
+        cr = self._download_file(INGREDIENTS_CSV_URL)
 
-    with open(save_path, "w+") as json_fp:
-        with open(csv_path) as csv_fp:
-            for entry in csv.DictReader(csv_fp.readlines()):
+        with open(save_path, "w+") as json_fp:
+            for p_e_id, ingr_id, inci_name in list(cr)[1:]:
                 json_entry = json.dumps(
                     {
-                        "p_e_id": int(entry["p_e_id"]),
-                        "ingr_id": int(entry["ingr_id"]),
-                        "inci_name": entry["inci_name"],
+                        "p_e_id": int(p_e_id),
+                        "ingr_id": int(ingr_id),
+                        "inci_name": inci_name,
                     }
                 )
                 json_fp.write(json_entry)
                 json_fp.write("\n")
 
+        logger.info(f"Finished downloading Ingredients data. Saved to {save_path}")
 
-convert_reviews_data_to_json()
+
+if __name__ == "__main__":
+    etl = DatazeitETL()
+    etl.run()
