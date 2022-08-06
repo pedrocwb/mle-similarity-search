45c7f41663d17844c4fc52723de1addc3b8d3f1e
pedro <pedro@datarevenue.com>
2022-08-06 19:06:30 -0300

    Update build.sh
    Took 4 minutes

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Creating conda environment"
+conda create -n datazeit-mle python=3.7
+
+echo "installing pip packages"
+pip install -r requirements/requirements.txt
+pip install -e datazeit/
 
 echo "Start ETL routine"
 python scripts/etl.py
